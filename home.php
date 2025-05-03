8af7b564a32b0411923045e01120bd3efb34ee74
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-05-02 23:51:32 -0700

    add footer secton

--- home.php
+++ home.php
@@ -5,12 +5,15 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Document</title>
    
+  
 
    <link rel="stylesheet" href="https://cdn.jsdelivr net/npm/swiper@11/swiper-bundle.min.css"/>
 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 
    <link rel="stylesheet" href="css/style.css">
+
+
 
 
    
@@ -36,7 +39,55 @@
 
 
 
-   <script src="https://cdn.jsdelivr.net/npm swiper@11/swiper-bundle.min.js"></script>
-   <script src="js/script.js  "></script>
+
+
+
+   <section class="footer">
+      <div class="box-container">
+         <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"><i class="fas fa-angle-right"></i> Home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i>packages</a>
+            <a href="contat.php"><i class="fas fa-angle-right"></i>contact</a>
+         </div>
+
+         <div class="box">
+            <h3>extra links</h3>
+            <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i>privacy  policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+            
+         </div>
+
+         <div class="box">
+            <h3>contact info</h3>
+            <a href="#"><i class="fas fa-phone"></i> +94782636481</a>
+            <a href="#"><i class="fas fa-phone"></i> +94719176816</a>
+            <a href="#"><i class="fas fa-envelope"></i> nirosh@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i> colombo,Sri Lanka</a>
+            
+            
+         </div>
+
+         <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook"></i> facebook</a>
+            <a href="#"><i class="fab fa-instagram"></i> intagram</a>
+            <a href="#"><i class="fab fa-twitter"></i> twitter</a>
+            <a href="#"><i class="fab fa-linkedin"></i> linkedin</a> 
+         </div>
+      </div>
+
+      <div class="credit">all right reserved @niroshEdirisinghe.</div>
+
+   </section>
+
+
+   <script src="script.js  "></script>
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+
+   
 </body>
 </html>

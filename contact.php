fa1988cd47781847f444a6087f0855a0f5d2da7b
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-05-03 16:01:00 -0700

    change swipper link

--- contact.php
+++ contact.php
@@ -89,8 +89,9 @@
    </section>
 
 
-   <script src="script.js  "></script>
-   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<script src="script.js  "></script>
 
    
 </body>

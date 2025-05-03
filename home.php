5719e563baa3c566821a6ca30825ea413792c2e6
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-05-02 20:13:39 -0700

    initial commit

---
+++ home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   
+
+   <link rel="stylesheet" href="https://cdn.jsdelivr net/npm/swiper@11/swiper-bundle.min.css"/>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+   <link rel="stylesheet" href="css/style.css">
+
+
+   
+</head>
+<body>
+   <section class="header">
+
+      <a href="home.php" class="logo">travel.</a>
+
+      <nav class="navbar">
+         <a href="home.php">Home</a>
+         <a href="about.php">about</a>
+         <a href="package.php">packages</a>
+         <a href="contat.php">contact</a>
+      </nav>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+   </section>
+
+   
+
+
+
+
+   <script src="https://cdn.jsdelivr.net/npm swiper@11/swiper-bundle.min.js"></script>
+   <script src="js/script.js  "></script>
+</body>
+</html>

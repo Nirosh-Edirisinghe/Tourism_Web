e4ba79c443629cfbe4adc8ee95da246f0abac5b0
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-05-05 00:21:34 -0700

    add contact section

--- contact.php
+++ contact.php
@@ -34,9 +34,33 @@
 
    </section>
 
-   <div class="heading">
-      <h1>about us</h1>
-   </div>
+   <section id="contact">
+      <h1 class="heading-title contact-1">contact us</h1>
+      <h1 class="head">Get In Touch!</h1>
+      <p class="sub-heading">We’re here to help you plan your perfect journey! Whether you have questions about our services, need travel advice, or want to customize your trip</p>
+
+      <div class="contact-form">
+         <form action="#">
+            <h1 class="head head-1">Need Support!</h1>
+            <p class="sub-heading para2">
+               Contact uss for a quoter, help to join the tem
+            </p>
+
+            <input type="text" class="input" placeholder="Your name">
+            <input type="text" class="input" placeholder="Your email">
+            <input type="text" class="input" placeholder="Your subject">
+            <textarea class="input" cols="30" rows="5"></textarea>
+            <input type="submit" class="input submit" value="send Message">
+         </form>
+
+         <div class="map-container">
+            <div class="mapBg"></div>
+            <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3961.081740063591!2d80.00014727373237!3d6.880810918910618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwNTInNTAuOSJOIDgwwrAwMCcwOS44IkU!5e0!3m2!1sen!2slk!4v1746420901927!5m2!1sen!2slk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+         </div>
+      </div>
+   </section>
 
    
 
